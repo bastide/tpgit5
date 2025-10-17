9ea203112687df7eb0d3df09d5c03ff3b1190c4e
Rémi Bastide <Remi.Bastide@gmail.com>
2025-10-17 17:21:48 +0200

    Modif à droite

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         // Print Hello, World! to the console
+        // Modif à droite pour conflit
         System.out.println("Hello, World!");
     }
 }
