07062ea61d196ef09566d6c9f6b58412fe0dc607
Rémi Bastide <Remi.Bastide@gmail.com>
2025-10-17 17:20:43 +0200

    commit à guache pour conflit

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         // Print Hello, World! to the console
+        // Modif à gauche
         System.out.println("Hello, World!");
     }
 }
